5bc292bda123a9e33718f95ae9db116b25d5d707
jaehue <jaehue@jang.io>
2015-08-30 22:30:22 +0900

    refactoring

--- router.go
+++ router.go
@@ -5,48 +5,25 @@ import (
 	"net/http"
 )
 
-type router struct {	
-	// key: URL Pattern
-	// value: URL Pattern별로 처리할 dispatcher
+type router struct {
+	// key: HTTP Method (GET|POST|PUT|PATCH|DELETE)
+	// value: HTTP Method별로 처리할 dispatcher
 	dispatchers       map[string]*dispatcher
 	staticFileHandler map[string]http.HandlerFunc
 }
 
 type dispatcher struct {
-	// key: Method (GET|POST|PUT|PATCH|DELETE)
-	// value:  method 별로 실행 할 HandlerFunc
+	// key: URL Pattern
+	// value:  URL Pattern별로 실행 할 HandlerFunc
 	handles map[string]HandlerFunc
 }
 
-// 요청 Method에 해당하는 HandlerFunc를 호출
-func (d *dispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fn, ok := d.handles[r.Method]
-	if !ok {
-		http.NotFound(w, r)
-		return
-	}
-
-	c := &Context{Params: make(map[string]interface{}), ResponseWriter: w, Request: r}
-	if params, ok := ctx.GetAll(r); ok {
-		for k, v := range params {
-			c.Params[k] = v
-		}
-	}
-
-	result := fn(c)
-	if renderer, ok := result.(renderer); ok {
-		renderer.render(w, r)
-		return
-	}
-	fmt.Fprint(w, result)
-}
-
 func (r *router) Get(path string, h HandlerFunc) {
-	r.register("GET", path, h)
+	r.HandleFunc("GET", path, h)
 }
 
 func (r *router) Post(path string, h HandlerFunc) {
-	r.register("POST", path, h)
+	r.HandleFunc("POST", path, h)
 }
 
 func (r *router) Static(path string) {
@@ -59,19 +36,40 @@ func (r *router) Static(path string) {
 	}
 }
 
-func (r *router) register(method, pattern string, h HandlerFunc) {
-	d, ok := r.dispatchers[pattern]
+func (r *router) HandleFunc(method, pattern string, h HandlerFunc) {
+	d, ok := r.dispatchers[method]
 	if !ok {
 		d = &dispatcher{make(map[string]HandlerFunc)}
-		r.dispatchers[pattern] = d
+		r.dispatchers[method] = d
 	}
-	d.handles[method] = h
+	d.handles[pattern] = h
 }
 
+// 요청 Method에 해당하는 HandlerFunc를 호출
 func (r *router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for pattern,dispatcher := range r.dispatchers{
-		if req.URL.Path == pattern {
-			dispatcher.ServeHTTP(w, req)
+	dispatcher, ok := r.dispatchers[req.Method]
+	if !ok {
+		http.NotFound(w, req)
+		return
+	}
+
+	fn, ok := dispatcher.handles[req.URL.Path]
+	if !ok {
+		http.NotFound(w, req)
+		return
+	}
+
+	c := &Context{Params: make(map[string]interface{}), ResponseWriter: w, Request: req}
+	if params, ok := ctx.GetAll(req); ok {
+		for k, v := range params {
+			c.Params[k] = v
 		}
 	}
+
+	result := fn(c)
+	if renderer, ok := result.(renderer); ok {
+		renderer.render(w, req)
+		return
+	}
+	fmt.Fprint(w, result)
 }
