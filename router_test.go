3b52acf892047b6869fab9ea6d72ad6a1c63e69b
jaehue <jaehue@jang.io>
2015-08-31 23:35:49 +0900

    add lookup test code

--- router_test.go
+++ router_test.go
@@ -34,4 +34,30 @@ func TestRouter(t *testing.T) {
 		So(ok, ShouldBeTrue)
 	})
 
+	Convey("Lookup", t, func() {
+		r := &router{dispatchers: make(map[string]*dispatcher)}
+
+		ok := false
+		r.HandleFunc("GET", "/users/:id/addresses", func(a *App) Result {
+			if a.Params["id"] == "1" {
+				ok = true
+			}
+			return nil
+		})
+
+		Convey("found", func() {
+			req, _ := http.NewRequest("GET", "/users/1/addresses", nil)
+			r.ServeHTTP(&mockResponseWriter{}, req)
+
+			So(ok, ShouldBeTrue)
+		})
+
+		Convey("not found", func() {
+			req, _ := http.NewRequest("GET", "/users/2/addresses", nil)
+			r.ServeHTTP(&mockResponseWriter{}, req)
+
+			So(ok, ShouldBeFalse)
+		})
+
+	})
 }
